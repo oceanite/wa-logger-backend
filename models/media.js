7324faea9b10d8b72126a151cf0e6417a1848ad3
Siti Zahra Ananda Kurniawan <162027699+szahraak@users.noreply.github.com>
2024-12-24 02:11:19 +0700

    Update media.js

--- models/media.js
+++ models/media.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const FileSchema = new mongoose.Schema({
     filename: { type: String, required: true },
-    updatedName: { type: String, required: true },
+    storedName: { type: String, required: true },
     path: { type: String, required: true }, // File path
     mimetype: { type: String, required: true }, // MIME type of the file
     size: { type: Number, required: true }, // File size in bytes
     uploadedAt: { type: Number, required: true },
     chatroomID: { type: String, required: true },
-    mediaKey: { type: String, required: true }
+    mediaKey: { type: String, required: true },
+    indexTotal: { type: Number },
+    fileIndex: { type: Number }
 });
 
 module.exports = mongoose.model('File', FileSchema);
